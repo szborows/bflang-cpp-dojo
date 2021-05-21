8184fd8991ed18825b2995473e288fcc97660f65
Tomasz Proczkowski <proczkowski.tomasz@gmail.com>
2021-05-21 11:07:31 +0200

    going back to cpp11 due to clang errors

--- include/stringConverter.hpp
+++ include/stringConverter.hpp
@@ -2,7 +2,7 @@
 #include <string>
 
 template <typename T, std::size_t N>
-auto convertToString(const T(&str)[N])
+std::string convertToString(const T(&str)[N])
 {
     std::string result;
     for (std::size_t i = 0; i < N - 1; ++i)
